4cedd14a5811ab18c97ba7bfd19811504b9f8678
Josh Berry <jberry@macim113-008.akdispatch.local>
2018-04-24 16:50:38 -0800

    consolidating code / cleanup

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -10,10 +10,29 @@ let card_names = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa
 
 const deck = document.getElementById('mainDeck');
 const reset_button = document.querySelector('.restart');
-let moves = document.querySelector('.moves');
+const moves = document.querySelector('.moves');
 
 reset_button.addEventListener('click', playGame);
 
+function createDeck() {
+    // clears deck to start before redrawing.
+    // Remove ChildrenNodes - StackOverflow --> https://bit.ly/2Hmw67R
+    while(deck.hasChildNodes() ){
+        deck.removeChild(deck.lastChild);
+    }
+
+    // Loops through the card_names array and recreates the card elements on the page.
+    for (let i = 0; i < card_names.length; i++) {
+
+        const newCard = document.createElement('li');
+        newCard.className = "card";
+        const newCardData = document.createElement('i');
+        newCardData.className = card_names[i];
+
+        const addNewCardData = newCard.appendChild(newCardData);
+        const addNewCard = deck.appendChild(newCard);
+    }
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -39,7 +58,24 @@ function shuffle(array) {
 }
 
 
-// adds click eventListener to <ul> rather than each individual card.
+    //  card.addEventListener('click', function(event) {
+    //     event.target.classList.toggle('open');
+    //     event.target.classList.toggle('show');
+    // });
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+ // adds click eventListener to <ul> rather than each individual card.
 deck.addEventListener('click', function(event) {
     move_count += 1;
     moves.innerHTML = move_count;
@@ -54,27 +90,6 @@ deck.addEventListener('click', function(event) {
 
     console.log(event.target);
 });
-
-
-    
-
-    //  card.addEventListener('click', function(event) {
-    //     event.target.classList.toggle('open');
-    //     event.target.classList.toggle('show');
-    // });
-
-    
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 //  increasing move counter
 // span .moves .innerHtml ++
@@ -91,28 +106,9 @@ reset_button.addEventListener('click', playGame);
 function playGame() {
     // shuffles the order of the cards to begin.
     shuffle(card_names);
-
     //Resets the move counter and displayed number of moves.
     move_count = 0;
     moves.innerHTML = 0;
-
-    // clears deck to start before redrawing.
-    // Remove ChildrenNodes - StackOverflow --> https://bit.ly/2Hmw67R
-    while(deck.hasChildNodes() ){
-        deck.removeChild(deck.lastChild);
-    }
-
-    // Loops through the card_names array and recreates the card elements on the page.
-    for (let i = 0; i < card_names.length; i++) {
-
-        const newCard = document.createElement('li');
-        newCard.className = "card";
-        const newCardData = document.createElement('i');
-        newCardData.className = card_names[i];
-
-        const addNewCardData = newCard.appendChild(newCardData);
-        const addNewCard = deck.appendChild(newCard);
-    }
+    createDeck();
 }
-
 
