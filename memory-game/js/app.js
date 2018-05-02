d02d3dc58abcd0280143a11df1ed361304ad3363
Josh Berry <jberry@macim113-007.akdispatch.local>
2018-05-02 13:29:43 -0800

    change back

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -30,7 +30,7 @@ reset_button.addEventListener('click', resetGame);
 play_again.addEventListener('click', playAgain);
 
 //display the matched pairs from the start
-matched_pairs = scoreDisplay.innerHTML;
+scoreDisplay.innerHTML = matched_pairs;
 
 playGame();
 
