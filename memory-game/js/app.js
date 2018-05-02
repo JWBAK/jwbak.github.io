81e824c32e6a891eb45b552f303841ec0ae37fd2
Josh Berry <jberry@macim113-007.akdispatch.local>
2018-05-02 13:57:31 -0800

    removing unneeded code.

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -188,7 +188,6 @@ function resetGame(){
   move_count = 0;
   moves.innerHTML = 0;
   matched_pairs = 0;
-  scoreDisplay.innerHTML = 0;
   starOne.style.display = "block";
   starTwo.style.display = "block";
   starThree.style.display = "block";
